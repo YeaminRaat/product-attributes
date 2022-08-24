ed167a72d4f51aaab259f2cb0af911ee5719204d
Yeamin Hossain <yeaminhossain2@gmail.com>
2022-08-24 16:58:29 +0600

    initial commit

---
+++ app/Models/Variant.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Variant extends Model
+{
+    protected $fillable = [
+        'title', 'description'
+    ];
+
+    public function productVariants()
+    {
+        return $this->hasMany(ProductVariant::class,'variant_id','id');
+    }
+
+}
